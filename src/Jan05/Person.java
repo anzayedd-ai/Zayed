941aefe589ef799fb1932f83f0f170898243d0cc
anzayedd-ai <anzayedd@gmail.com>
2026-01-05 17:25:38 +0600

    Jan05

--- src/Jan05/Person.java
+++ src/Jan05/Person.java
@@ -6,14 +6,12 @@ public class Person {
     String name;
     Double salary;
 
-    // Constructor
     public Person(Integer id, String name, Double salary) {
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
 
-    // Display method
     public void display() {
         System.out.println(id + " " + name + " " + salary);
     }
